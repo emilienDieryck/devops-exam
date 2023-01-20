98f0246464401a057ac77e69c87dd2d478ec47b8
Emilien <emilien.dieryck@student.vinci.be>
2023-01-20 10:43:18 +0100

    test i. final

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -18,10 +18,19 @@ describe("validations tests suites - isEmpty", () => {
 });
 
 describe("validations tests suites - eightCaracter", () => {
-    test("should return true as the eightCaracter ", () => {
+    test("should return false if the lenght <8 ", () => {
         const result = eigtCaracter("test");
+        expect(result).toBe(false);
+    });
+    test("should return true if the game Caracter = 8 ", () => {
+        const result = eigtCaracter("gameCara");
         expect(result).toBe(true);
     });
+    test("should return true if the game Caracter > 8", () => {
+        const result = eigtCaracter("test9àéekks");
+        expect(result).toBe(true);
+    });
+
 });
 
 // TODO: Create tests suite for validation function
--- src/services/validations.js
+++ src/services/validations.js
@@ -2,7 +2,7 @@
 
 const isEmpty = (label) => !label || label.length === 0;
 
-const eigtCaracter = (label) => true;
+const eigtCaracter = (label) => label.length >= 8;
 
 exports.isEmpty = isEmpty;
 exports.eigtCaracter = eigtCaracter;
