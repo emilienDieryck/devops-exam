d99d1478e1c02fef84967f066e07f7e51e1d76f5
Emilien <emilien.dieryck@student.vinci.be>
2023-01-20 11:00:01 +0100

    test ii.

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,4 +1,4 @@
-const { isEmpty, eigtCaracter } = require("../validations");
+const { isEmpty, eigtCaracter, oneSpecialCara } = require("../validations");
 
 describe("validations tests suites - isEmpty", () => {
     test("should return true as the label is undefined", () => {
@@ -30,7 +30,21 @@ describe("validations tests suites - eightCaracter", () => {
         const result = eigtCaracter("test9àéekks");
         expect(result).toBe(true);
     });
+});
 
+describe("validations tests suites - oneSpecialCara", () => {
+    test("should return false if the no special cara ", () => {
+        const result = eigtCaracter("test");
+        expect(result).toBe(false);
+    });
+    test("should return true if the game specialcara = 8 ", () => {
+        const result = eigtCaracter("gameCaraK+");
+        expect(result).toBe(true);
+    });
+    test("should return true if the game Caracter > 8", () => {
+        const result = eigtCaracter("test9[éekks");
+        expect(result).toBe(true);
+    });
 });
 
 // TODO: Create tests suite for validation function
--- src/services/validations.js
+++ src/services/validations.js
@@ -4,5 +4,22 @@ const isEmpty = (label) => !label || label.length === 0;
 
 const eigtCaracter = (label) => label.length >= 8;
 
+const oneSpecialCara = (label) => {
+    for (let index = 0; index < label.length; index++) {
+        const compt = 0;
+        const element = label[index];
+        if ((compt = 1)) {
+            return true;
+        }
+        if (
+            element ===
+            /[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~\`\_\-]/g
+        )
+            compt += 1;
+    }
+    return false;
+};
+
 exports.isEmpty = isEmpty;
 exports.eigtCaracter = eigtCaracter;
+exports.oneSpecialCara = oneSpecialCara;
